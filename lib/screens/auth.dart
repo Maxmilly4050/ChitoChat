f6b2a909f9f948e2df27dc6878682c5b5c9449c7
Maxmilly4050 <maxsteel4050@gmail.com>
2026-04-08 21:44:07 +0300

    setup of the login screen

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -14,16 +14,55 @@ class _AuthScreenState extends State<AuthScreen> {
       backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       body: Center(
         child: SingleChildScrollView(
-          child: Container(
-            margin: const EdgeInsets.only(
-              left: 20,
-              right: 20,
-              top: 30,
-              bottom: 20,
-            ),
-            width: 200,
-            child: Image(image: image),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 30,
+                  bottom: 20,
+                ),
+                width: 200,
+                child: Image(image: AssetImage('assets/images/chat.png')),
+              ),
+              Card(
+                margin: EdgeInsets.all(20),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Form(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextFormField(
+                            decoration: InputDecoration(labelText: 'Email'),
+                            keyboardType: TextInputType.emailAddress,
+                            autocorrect: false,
+                            textCapitalization: TextCapitalization.none,
+                          ),
+                          SizedBox(height: 16),
+                          TextFormField(
+                            decoration: InputDecoration(labelText: 'Password'),
+                            obscureText: true,
+                          ),
+                          SizedBox(height: 24),
+                          ElevatedButton(
+                            onPressed: () {
+                              // Handle login logic here
+                            },
+                            child: Text('Login'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
+          
         )
       ),
     );
