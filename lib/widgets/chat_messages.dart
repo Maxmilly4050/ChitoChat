37e8ef457b593749bb09dfd31e805be015937a39
Maxmilly4050 <maxsteel4050@gmail.com>
2026-04-10 22:19:40 +0300

    Displaying sent messages

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,6 +6,29 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('chat').orderBy('createdAt', descending: true).snapshots(), 
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(child: Text('No messages found.'));
+        }
+
+        if (snapshot.hasError) {
+          return const Center(child: Text('An error occurred while loading messages.'));
+        }
+
+        final chatDocs = snapshot.data!.docs;
+        return ListView.builder(
+          padding: const EdgeInsets.only(top: 20.0, left: 20.0, right: 10.0, bottom: 1.0),
+          reverse: true,
+          itemCount: chatDocs.length,
+          itemBuilder: (context, index) => Text(chatDocs[index].data()['text']),
+        );
+      },
+      );
   }
 }
