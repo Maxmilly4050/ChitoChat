93f5f175f822ef34775515dd599e08a7c31293e8
Maxmilly4050 <maxsteel4050@gmail.com>
2026-04-11 15:25:48 +0300

    Recieving push notifications using firebase messaging (manually)

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -2,9 +2,28 @@ import 'package:chito_chat/widgets/chat_messages.dart';
 import 'package:chito_chat/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    fcm.requestPermission();
+
+    fcm.subscribeToTopic('chito_chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
